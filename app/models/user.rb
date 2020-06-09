f7d8ca849492169714c9c58f5f0aa649a42eb000
marie lebon <marie.lebon@kedgebs.com>
2020-06-09 16:00:01 +0200

    devise done

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :race_number, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+end
