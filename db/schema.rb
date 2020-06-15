3e04661d65d5c4e02bdf19c296a6b4d5f72e93db
Margaux Le Gall <margauxxlegall@gmail.com>
2020-06-15 11:15:49 +0200

    uptodate or not?

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_06_12_061317) do
 
   # These are extensions that must be enabled in order to support this database
@@ -95,6 +94,7 @@ ActiveRecord::Schema.define(version: 2020_06_12_061317) do
     t.string "username"
     t.integer "race_number"
     t.string "location"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
