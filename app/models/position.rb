010b812c2e913ab566f72798421be0fef2ba4203
Pauline Poize <pauline.poize@kedgebs.com>
2020-06-15 20:12:27 +0200

    Work in progress

--- app/models/position.rb
+++ app/models/position.rb
@@ -4,4 +4,13 @@ class Position < ApplicationRecord
   validates :point, presence: true
   validates :time, presence: true
   validates :place, presence: true, uniqueness: true
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    associated_against: {
+      user_id: [ :first_name, :last_name, :race_number ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
+  # multisearchable :against => [:user, :author]
 end
