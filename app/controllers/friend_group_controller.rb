e9028876d34c1e504f4d9a871da6634ec58870f3
Pauline Poize <pauline.poize@kedgebs.com>
2020-06-18 04:15:29 +0200

    m

--- app/controllers/friend_group_controller.rb
+++ app/controllers/friend_group_controller.rb
@@ -1,9 +1,9 @@
 class FriendGroupController < ApplicationController
   def index
-    if params[:event] && params[:first_query].empty? == false && params[:second_query].empty? == false && params[:third_query].empty? && params[:fourth_query].empty? && params[:fifth_query].empty? && params[:sixth_query].empty? && params[:seventh_query].empty?
+    if (params[:event] && params[:first_query] && params[:second_query] && params[:third_query].present? == false) || (params[:event] && params[:first_query] && params[:second_query] && params[:third_query].empty?)
       user1 = User.search_by_names_and_race_number(params[:first_query]).first
       user2 = User.search_by_names_and_race_number(params[:second_query]).first
-      event = Event.search_by_title(params[:event]).first
+      event = Event.find(params[:event])
       @friend_group = FriendGroup.new(
         created_by_id: current_user.id,
         event_id: event.id
@@ -19,13 +19,13 @@ class FriendGroupController < ApplicationController
         user_id: user2.id
         )
       @friend_groups_users.save!
-      @friend_groups = FriendGroup.where(created_by_id: current_user)
+      @friend_groups = FriendGroup.where(created_by_id: current_user).order('created_at DESC')
       return @friend_groups
-    elsif params[:event] && params[:first_query].empty? == false && params[:second_query].empty? == false && params[:third_query].empty? == false && params[:fourth_query].empty? && params[:fifth_query].empty? && params[:sixth_query].empty? && params[:seventh_query].empty?
+    elsif params[:event] && params[:first_query] && params[:second_query] && params[:third_query] && params[:fourth_query].empty? 
       user1 = User.search_by_names_and_race_number(params[:first_query]).first
       user2 = User.search_by_names_and_race_number(params[:second_query]).first
       user3 = User.search_by_names_and_race_number(params[:third_query]).first
-      event = Event.search_by_title(params[:event]).first
+      event = Event.find(params[:event])
       @friend_group = FriendGroup.new(
         created_by_id: current_user.id,
         event_id: event.id
@@ -46,14 +46,14 @@ class FriendGroupController < ApplicationController
         user_id: user3.id
         )
       @friend_groups_users.save!
-      @friend_groups = FriendGroup.where(created_by_id: current_user)
+      @friend_groups = FriendGroup.where(created_by_id: current_user).order('created_at DESC')
       return @friend_groups
-    elsif params[:event] && params[:first_query].empty? == false && params[:second_query].empty? == false && params[:third_query].empty? == false && params[:fourth_query].empty? == false && params[:fifth_query].empty? && params[:sixth_query].empty? && params[:seventh_query].empty?
+    elsif params[:event] && params[:first_query] && params[:second_query] && params[:third_query] && params[:fourth_query] && params[:fifth_query].empty? 
       user1 = User.search_by_names_and_race_number(params[:first_query]).first
       user2 = User.search_by_names_and_race_number(params[:second_query]).first
       user3 = User.search_by_names_and_race_number(params[:third_query]).first
       user4 = User.search_by_names_and_race_number(params[:fourth_query]).first
-      event = Event.search_by_title(params[:event]).first
+      event = Event.find(params[:event])
       @friend_group = FriendGroup.new(
         created_by_id: current_user.id,
         event_id: event.id
@@ -78,15 +78,15 @@ class FriendGroupController < ApplicationController
         user_id: user4.id
         )
       @friend_groups_users.save!
-      @friend_groups = FriendGroup.where(created_by_id: current_user)
+      @friend_groups = FriendGroup.where(created_by_id: current_user).order('created_at DESC')
       return @friend_groups
-    elsif params[:event] && params[:first_query].empty? == false && params[:second_query].empty? == false && params[:third_query].empty? == false && params[:fourth_query].empty? == false && params[:fifth_query].empty? == false && params[:sixth_query].empty? && params[:seventh_query].empty?
+    elsif params[:event] && params[:first_query] && params[:second_query] && params[:third_query] && params[:fourth_query] && params[:fifth_query] && params[:sixth_query].empty? 
       user1 = User.search_by_names_and_race_number(params[:first_query]).first
       user2 = User.search_by_names_and_race_number(params[:second_query]).first
       user3 = User.search_by_names_and_race_number(params[:third_query]).first
       user4 = User.search_by_names_and_race_number(params[:fourth_query]).first
       user5 = User.search_by_names_and_race_number(params[:fifth_query]).first
-      event = Event.search_by_title(params[:event]).first
+      event = Event.find(params[:event])
       @friend_group = FriendGroup.new(
         created_by_id: current_user.id,
         event_id: event.id
@@ -117,16 +117,16 @@ class FriendGroupController < ApplicationController
         user_id: user5.id
         )
       @friend_groups_users.save!      
-      @friend_groups = FriendGroup.where(created_by_id: current_user)
+      @friend_groups = FriendGroup.where(created_by_id: current_user).order('created_at DESC')
       return @friend_groups
-    elsif params[:event] && params[:first_query].empty? == false && params[:second_query].empty? == false && params[:third_query].empty? == false && params[:fourth_query].empty? == false && params[:fifth_query].empty? == false && params[:sixth_query].empty? == false && params[:seventh_query].empty?
+    elsif params[:event] && params[:first_query] && params[:second_query] && params[:third_query] && params[:fourth_query] && params[:fifth_query] && params[:sixth_query] && params[:seventh_query].empty?
       user1 = User.search_by_names_and_race_number(params[:first_query]).first
       user2 = User.search_by_names_and_race_number(params[:second_query]).first
       user3 = User.search_by_names_and_race_number(params[:third_query]).first
       user4 = User.search_by_names_and_race_number(params[:fourth_query]).first
       user5 = User.search_by_names_and_race_number(params[:fifth_query]).first
       user6 = User.search_by_names_and_race_number(params[:sixth_query]).first      
-      event = Event.search_by_title(params[:event]).first
+      event = Event.find(params[:event])
       @friend_group = FriendGroup.new(
         created_by_id: current_user.id,
         event_id: event.id
@@ -162,9 +162,9 @@ class FriendGroupController < ApplicationController
         user_id: user6.id
         )
       @friend_groups_users.save!      
-      @friend_groups = FriendGroup.where(created_by_id: current_user)
+      @friend_groups = FriendGroup.where(created_by_id: current_user).order('created_at DESC')
       return @friend_groups
-    elsif params[:event] && params[:first_query].empty? == false && params[:second_query].empty? == false && params[:third_query].empty? == false && params[:fourth_query].empty? == false && params[:fifth_query].empty? == false && params[:sixth_query].empty? == false && params[:seventh_query].empty? == false
+    elsif params[:event] && params[:first_query] && params[:second_query] && params[:third_query] && params[:fourth_query] && params[:fifth_query] && params[:sixth_query] && params[:seventh_query]
       user1 = User.search_by_names_and_race_number(params[:first_query]).first
       user2 = User.search_by_names_and_race_number(params[:second_query]).first
       user3 = User.search_by_names_and_race_number(params[:third_query]).first
@@ -172,7 +172,7 @@ class FriendGroupController < ApplicationController
       user5 = User.search_by_names_and_race_number(params[:fifth_query]).first
       user6 = User.search_by_names_and_race_number(params[:sixth_query]).first      
       user7 = User.search_by_names_and_race_number(params[:seventh_query]).first      
-      event = Event.search_by_title(params[:event]).first
+      event = Event.find(params[:event])
       @friend_group = FriendGroup.new(
         created_by_id: current_user.id,
         event_id: event.id
@@ -213,10 +213,10 @@ class FriendGroupController < ApplicationController
         user_id: user7.id
         )
       @friend_groups_users.save!        
-      @friend_groups = FriendGroup.where(created_by_id: current_user)
+      @friend_groups = FriendGroup.where(created_by_id: current_user).order('created_at DESC')
       return @friend_groups
     else
-      @friend_groups = FriendGroup.where(created_by_id: current_user)
+      @friend_groups = FriendGroup.where(created_by_id: current_user).order('created_at DESC')
     end
   end
 end
